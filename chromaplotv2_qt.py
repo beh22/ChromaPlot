173191b911483e6994c7198b265d71bc6425b244
beh22 <beh22@ic.ac.uk>
2024-08-09 12:07:11 +0100

    Update chromaplotv2_qt.py
    Added select curves in separate dialog, added options to change styling, improved small things. Have started adding ability to change UV curve styling

--- chromaplotv2_qt.py
+++ chromaplotv2_qt.py
@@ -2,7 +2,7 @@ import sys
 import AKdatafile as AKdf
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QDialog, QFileDialog,
-    QMessageBox, QCheckBox, QLabel, QDialogButtonBox, QLineEdit
+    QMessageBox, QCheckBox, QLabel, QDialogButtonBox, QLineEdit, QColorDialog, QComboBox, QDoubleSpinBox
 )
 from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtGui import QFont
@@ -22,10 +22,22 @@ plt.rcParams['font.family'] = "sans-serif"
 '''
 To do:
 - Give error rather than quitting when options are tried with no data loaded
-- Sort formatting of curve checkboxes - move into new dialog?, easier to add options for color, ls etc.
 - Remove first and waste fractions
 - Tabs
-- Add run log as annotations
+- Add run log as annotations, and don't allow run log, fraction etc. curves to be added to checkboxes
+- Add color and linewidth options to curves
+- Add limits to options
+- Different colors for each shading area/labels?
+- Sheeps
+- Shade volume option for when fractions aren't present
+- Change fonts?
+- Custom y labels
+- More complete error messages
+- Change locations that extra dialogs open - not over the plot!
+- Sort fraction label appearance
+- Add UV to select curves dialog, no checkbox, but other options
+
+excluded_curves = {'UV', 'Injection', 'Run Log', 'Fraction', 'UV_CUT_TEMP@100,BASEM'}
 
 '''
 
@@ -141,6 +153,130 @@ class OptionsDialog(QDialog):
         self.add_fraction_labels_checkbox.setChecked(options.get('Add fraction labels', False))
         self.add_legend_checkbox.setChecked(options.get('Add Legend', False))
 
+class SelectCurvesDialog(QDialog):
+    curveOptionsChanged = pyqtSignal(dict)
+
+    def __init__(self, curves, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Select Curves")
+
+        self.curves = curves
+        self.parent = parent
+
+        self.main_layout = QVBoxLayout()
+        self.checkboxes = {}
+        self.curve_options = {}
+        excluded_curves = {'Injection', 'Run Log', 'Fraction', 'UV_CUT_TEMP@100,BASEM'}
+
+        # Controls for UV curve
+        self.setup_uv_controls()
+
+        for curve in self.curves:
+            if curve not in excluded_curves and curve != 'UV':
+                self.setup_curve_controls(curve)
+
+        self.setLayout(self.main_layout)
+
+    def setup_uv_controls(self):
+        # Add linestyle options for UV
+        linestyle_combo = QComboBox()
+        linestyle_combo.addItems(['-', '--', '-.', ':'])
+        linestyle_combo.setCurrentIndex(0)
+        linestyle_combo.currentIndexChanged.connect(lambda index, c='UV': self.handle_linestyle_change(c)(index))
+
+        # Add linewidth options for UV
+        linewidth_box = QDoubleSpinBox()
+        linewidth_box.setRange(0.5, 5.0)
+        linewidth_box.setSingleStep(0.5)
+        linewidth_box.setValue(1.0)
+        linewidth_box.valueChanged.connect(lambda value, c='UV': self.handle_linewidth_change(c)(value))
+
+        # Add color picker for UV
+        color_button = QPushButton("Select Color for UV")
+        color_button.clicked.connect(lambda _, c='UV': self.handle_color_change(c)())
+
+        # Layout for UV controls
+        uv_layout = QHBoxLayout()
+        uv_layout.addWidget(QLabel('UV Curve Style:'))
+        uv_layout.addWidget(linestyle_combo)
+        uv_layout.addWidget(linewidth_box)
+        uv_layout.addWidget(color_button)
+        self.main_layout.addLayout(uv_layout)
+
+        # Initialize UV curve options
+        self.curve_options['UV'] = {'linestyle': '-', 'linewidth': 1.0, 'color': 'black'}
+
+    def setup_curve_controls(self, curve):
+        checkbox = QCheckBox(curve)
+        checkbox.setChecked(False)
+        checkbox.stateChanged.connect(self.update_curve_selection)
+
+        # Add linestyle options
+        linestyle_combo = QComboBox()
+        linestyle_combo.addItems(['-', '--', '-.', ':'])
+        linestyle_combo.setCurrentIndex(0)
+        linestyle_combo.currentIndexChanged.connect(lambda index, c=curve: self.handle_linestyle_change(c)(index))
+
+        # Add linewidth options
+        linewidth_box = QDoubleSpinBox()
+        linewidth_box.setRange(0.5, 5.0)
+        linewidth_box.setSingleStep(0.5)
+        linewidth_box.setValue(1.0)
+        linewidth_box.valueChanged.connect(lambda value, c=curve: self.handle_linewidth_change(c)(value))
+
+        # Add color picker button
+        color_button = QPushButton(f"Select Color for {curve}")
+        color_button.clicked.connect(lambda _, c=curve: self.handle_color_change(c)())
+
+        # Layout for the curve options
+        hbox = QHBoxLayout()
+        hbox.addWidget(checkbox)
+        hbox.addWidget(linestyle_combo)
+        hbox.addWidget(linewidth_box)
+        hbox.addWidget(color_button)
+
+        self.main_layout.addLayout(hbox)
+        self.checkboxes[curve] = checkbox
+        self.curve_options[curve] = {'linestyle': '-', 'linewidth': 1.0, 'color': 'black'}
+
+
+    def handle_linestyle_change(self, curve):
+        def inner_handle_linestyle_change(index):
+            combo = self.sender()
+            if combo and combo.currentIndex() == index:
+                self.curve_options[curve]['linestyle'] = combo.currentText()
+            self.update_curve_selection(None)
+        return inner_handle_linestyle_change
+
+    def handle_linewidth_change(self, curve):
+        def inner_handle_linewidth_change(value):
+            spinbox = self.sender()
+            if spinbox and spinbox.value() == value:
+                self.curve_options[curve]['linewidth'] = value
+            self.update_curve_selection(None)
+        return inner_handle_linewidth_change
+
+    def handle_color_change(self, curve):
+        def inner_handle_color_change():
+            color = QColorDialog.getColor(Qt.black, self, "Select Color for " + curve)  # Default to black
+            if color.isValid():
+                color_name = color.name()
+                print(f"Color selected for {curve}: {color_name}")  # Debug print statement
+                self.curve_options[curve]['color'] = color_name
+                self.update_curve_selection(None)
+            else:
+                print("No valid color selected")  # Debug print statement
+        return inner_handle_color_change
+
+    def update_curve_selection(self, state):
+        selected_curves = {}
+
+        for curve, checkbox in self.checkboxes.items():
+            if checkbox.isChecked():
+                selected_curves[curve] = self.curve_options[curve]
+
+        self.curveOptionsChanged.emit(selected_curves)
+
 
 class SingleMode(QDialog):
     def __init__(self, mode_name, parent=None):
@@ -150,7 +286,6 @@ class SingleMode(QDialog):
         self.mode_name = mode_name
         self.loaded_file = None
         self.data = None
-        self.colors = ['r', 'g', 'b', 'c', 'm']
         self.show_legend = False
         self.show_fraction_labels = False
         self.options_state = {
@@ -158,8 +293,13 @@ class SingleMode(QDialog):
             'Add Legend': False
         }
 
+        self.colors = ['r', 'g', 'b', 'c', 'm']
+
         self.shaded_regions = []
         self.show_shaded_fractions = False
+        self.selected_curves = {}
+
+        self.select_curves_dialog = None 
 
         # Create layouts
         self.main_layout = QVBoxLayout()
@@ -172,6 +312,7 @@ class SingleMode(QDialog):
         self.clear_data_button = QPushButton("Clear data")
         self.save_plot_button = QPushButton("Save plot")
         self.options_button = QPushButton("Options")
+        self.select_curves_button = QPushButton("Select Curves")
         self.analyse_button = QPushButton("Analyse")
         self.back_button = QPushButton("Back")
 
@@ -180,11 +321,12 @@ class SingleMode(QDialog):
         self.button_layout.addWidget(self.clear_data_button)
         self.button_layout.addWidget(self.save_plot_button)
         self.button_layout.addWidget(self.options_button)
+        self.button_layout.addWidget(self.select_curves_button)
         self.button_layout.addWidget(self.analyse_button)
         self.button_layout.addWidget(self.back_button)
 
         # Create a matplotlib figure and canvas
-        self.figure = plt.figure(figsize=(14,7))
+        self.figure = plt.figure(figsize=(7,3.5))
         self.canvas = FigureCanvas(self.figure)
 
         # Add the canvas and checkbox layout to the side layout
@@ -203,6 +345,7 @@ class SingleMode(QDialog):
         self.clear_data_button.clicked.connect(self.clear_data)
         self.save_plot_button.clicked.connect(self.save_plot)
         self.options_button.clicked.connect(self.open_options_dialog)
+        self.select_curves_button.clicked.connect(self.open_select_curves_dialog)
         self.analyse_button.clicked.connect(self.open_analyse_dialog)
         self.back_button.clicked.connect(self.close_dialog)
 
@@ -215,6 +358,9 @@ class SingleMode(QDialog):
             if reply == QMessageBox.No:
                 return
         
+        # Clear the existing data before loading new data
+        self.clear_data()
+
         options = QFileDialog.Options()
         options |= QFileDialog.ReadOnly
         file_name, _ = QFileDialog.getOpenFileName(
@@ -226,34 +372,32 @@ class SingleMode(QDialog):
             print(f"File loaded: {file_name}")
             self.loaded_file = file_name
             self.data = AKdf.AKdatafile(file_name).genAKdict(1, 2)
-            self.create_checkboxes()
+
+            # Reopen the SelectCurvesDialog with the new data
+            self.open_select_curves_dialog()
+
+            # Update the plot with the new data
             self.update_plot()
 
-    def create_checkboxes(self):
-        # Remove old checkboxes
-        for i in reversed(range(self.checkbox_layout.count())):
-            widget = self.checkbox_layout.itemAt(i).widget()
-            if widget and widget not in [self.load_data_button, self.clear_data_button, self.save_plot_button, self.back_button]:
-                widget.setParent(None)
+    def open_select_curves_dialog(self):
+        if self.data is not None:
+            if self.select_curves_dialog is not None:
+                self.select_curves_dialog.close()
 
-        # Create new checkboxes for the curves
-        self.checkboxes = {}
-        self.checkbox_layout.addWidget(QLabel("Curves"))
-        for curve in self.data.keys():
-            if curve != 'UV':  # Exclude 'UV' since it's already plotted
-                checkbox = QCheckBox(curve)
-                checkbox.setChecked(False)  # Default to unchecked
-                checkbox.stateChanged.connect(self.update_plot)
-                self.checkbox_layout.addWidget(checkbox)
-                self.checkboxes[curve] = checkbox
+            self.select_curves_dialog = SelectCurvesDialog(self.data.keys(), self)
+            self.select_curves_dialog.show()
 
-        # Update the plot with current checkbox states
+            # Connect the signal for updating the plot with selected curves
+            self.select_curves_dialog.curveOptionsChanged.connect(self.update_selected_curves)
+
+    def update_selected_curves(self, selected_curves):
+        self.selected_curves = selected_curves
         self.update_plot()
 
     def update_plot(self):
         self.figure.clear()
         ax = self.figure.add_subplot(111)
-        
+
         self.fraction_labels = []
         self.ax1 = ax
 
@@ -273,12 +417,15 @@ class SingleMode(QDialog):
             handles.append(uv_line)
             labels.append('UV')
 
-        # Plot selected curves
+        # Plot selected curves with customization options
         y_axes = [ax]
 
-        for i, (curve, checkbox) in enumerate(self.checkboxes.items()):
-            if checkbox.isChecked() and curve in self.data:
-                color = self.colors[i % len(self.colors)]
+        for i, (curve, options) in enumerate(self.selected_curves.items()):
+            if curve in self.data:
+                linestyle = options.get('linestyle', '-')
+                linewidth = options.get('linewidth', 1)
+                color = options.get('color', self.colors[i % len(self.colors)])
+
                 curvekeys = list(self.data[curve].keys())
                 xunits = curvekeys[0]
                 yunits = curvekeys[1]
@@ -293,7 +440,10 @@ class SingleMode(QDialog):
                     new_ax.spines['right'].set_position(('outward', 40 * len(y_axes)))
                     y_axes.append(new_ax)
 
-                line, = new_ax.plot(x, y, label=curve, color=color)
+                try:
+                    line, = new_ax.plot(x, y, label=curve, color=color, linestyle=linestyle, linewidth=linewidth)
+                except Exception as e:
+                    print("Error plotting curve:", e)
                 new_ax.set_ylabel(curve, color=color)
                 new_ax.tick_params(axis='y', labelcolor=color)
 
@@ -313,13 +463,23 @@ class SingleMode(QDialog):
             self.add_fractions()
 
         if self.show_shaded_fractions:
-            for start_frac, stop_frac in self.shaded_regions:
-                self._shade_fractions(start_frac, stop_frac)
+            self.add_shaded_regions()
 
-      
         plt.tight_layout()
-
         self.canvas.draw()
+
+
+    def clear_data(self):
+        if self.loaded_file:
+            self.loaded_file = None
+            self.data = None
+            self.selected_curves = {}
+            self.figure.clear()
+            if self.select_curves_dialog is not None:
+                self.select_curves_dialog.close()
+                self.select_curves_dialog = None
+            print("All data cleared.")
+            self.canvas.draw()
 
     def add_fractions(self, stript=True, fontsize=6, labheight=5):
         flabx = []
@@ -350,27 +510,6 @@ class SingleMode(QDialog):
 
             label = self.ax1.text(flabx[i], labheight, flab[i], fontsize=fontsize, ha='center')
             self.fraction_labels.append(label)
-
-    def clear_data(self):
-        reply = QMessageBox.question(
-            self, 'Clear Data',
-            'Are you sure you want to clear the data?',
-            QMessageBox.Yes | QMessageBox.No, QMessageBox.No
-        )
-
-        if reply == QMessageBox.Yes:
-            if self.loaded_file:
-                self.loaded_file = None
-                self.data = None
-                self.figure.clear()
-                for i in reversed(range(self.checkbox_layout.count())):
-                    widget = self.checkbox_layout.itemAt(i).widget()
-                    if widget and widget not in [self.load_data_button, self.clear_data_button, self.save_plot_button, self.back_button]:
-                        widget.setParent(None)
-                print("All data cleared.")
-                self.canvas.draw()
-        else:
-            print("Data clearing canceled.")
 
     def save_plot(self):
         options = QFileDialog.Options()
@@ -407,7 +546,7 @@ class SingleMode(QDialog):
         self.show_fraction_labels = visible
         self.update_plot()
 
-    def set_shaded_fractions_visibility(self, start_frac, stop_frac, color='grey', alpha=0.5):
+    def set_shaded_fractions_visibility(self, start_frac, stop_frac):
         try:
             # Retrieve fraction data
             fractions = self.data['Fraction']['Fraction']
@@ -424,42 +563,28 @@ class SingleMode(QDialog):
             QMessageBox.warning(self, "Error", "Specified fractions are not in the data.")
             return
 
-        # Get the indices of the start and stop fractions
+        # Get the corresponding volume ranges
         start_index = fractions.index(start_frac)
         stop_index = fractions.index(stop_frac)
-
-        # Get the corresponding volume ranges
         start_vol = volumes[start_index]
         stop_vol = volumes[stop_index]
 
-        # Save the shaded region for potential future use
-        self.shaded_regions.append((start_frac, stop_frac))  # Save the fraction range
+        # Store the actual volumes for shading
+        self.shaded_regions.append((start_vol, stop_vol))  # Store volumes instead of indices
         self.show_shaded_fractions = True
 
-        # Apply shading
-        self._shade_fractions(start_frac, stop_frac, color=color, alpha=alpha)
+        # Update the plot
+        self.update_plot()
 
-    def _shade_fractions(self, start_frac, stop_frac, color='grey', alpha=0.5):
-        # Retrieve fraction data
-        fractions = self.data['Fraction']['Fraction']
-        volumes = self.data['Fraction']['ml']
+    def add_shaded_regions(self):
+        if not self.shaded_regions:
+            return
         
-        # Convert fraction numbers to integers
-        fractions = [int(x.strip("T\"")) for x in fractions if x.strip("T\"").isdigit()]
-
-        # Get the indices of the start and stop fractions
-        start_index = fractions.index(start_frac)
-        stop_index = fractions.index(stop_frac)
-
-        # Get the corresponding volume ranges
-        start_vol = volumes[start_index]
-        stop_vol = volumes[stop_index]
-
-        # Shade the area under the curve between the start and stop volumes
-        xdata, ydata = self.ax1.lines[0].get_data()  # Assuming the UV curve is the first plotted line
-        mask = (xdata >= start_vol) & (xdata <= stop_vol)
-        self.ax1.fill_between(xdata[mask], ydata[mask], color=color, alpha=alpha)
-        self.canvas.draw()
+        for start_vol, stop_vol in self.shaded_regions:
+            # Shade the area under the curve between the start and stop volumes
+            xdata, ydata = self.ax1.lines[0].get_data()  # Assuming the UV curve is the first plotted line
+            mask = (xdata >= start_vol) & (xdata <= stop_vol)
+            self.ax1.fill_between(xdata[mask], ydata[mask], color='grey', alpha=0.5)
 
     def undo_shade(self):
         if self.shaded_regions:
@@ -476,7 +601,24 @@ class SingleMode(QDialog):
 
 
 class OverlayMode(QDialog):
-    pass
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Overlay Mode")
+
+        # Create layout
+        self.layout = QVBoxLayout()
+
+        self.layout.addWidget(QLabel("Overlay Mode"))
+        self.layout.addWidget(QLabel("Functionality yet to be added"))
+
+        # Add buttons
+        self.button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        self.button_box.accepted.connect(self.accept)
+        self.button_box.rejected.connect(self.reject)
+        self.layout.addWidget(self.button_box)
+
+        # Set the layout
+        self.setLayout(self.layout)
 
 class MainWindow(QMainWindow):
     def __init__(self):
